e0e6e294fed403765345e4649d491e1f90e258c6
AndyLPK247 <AndyLPK247@gmail.com>
2019-08-31 20:33:54 -0400

    added DuckDuckGoSearchPage stub

---
+++ pages/search.py
@@ -0,0 +1,18 @@
+"""
+This module contains DuckDuckGoSearchPage,
+the page object for the DuckDuckGo search page.
+"""
+
+
+class DuckDuckGoSearchPage:
+
+  def __init__(self, browser):
+    self.browser = browser
+
+  def load(self):
+    # TODO
+    pass
+
+  def search(self, phrase):
+    # TODO
+    pass
