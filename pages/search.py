706e9287a47f0677178e570d12a43e8dea53ce14
AndyLPK247 <AndyLPK247@gmail.com>
2019-09-04 20:06:37 -0400

    implemented search methods

--- pages/search.py
+++ pages/search.py
@@ -4,19 +4,29 @@ the page object for the DuckDuckGo search page.
 """
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class DuckDuckGoSearchPage:
 
+  # URL
+
+  URL = 'https://www.duckduckgo.com'
+
+  # Locators
+
   SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+
+  # Initializer
 
   def __init__(self, browser):
     self.browser = browser
 
+  # Interaction Methods
+
   def load(self):
-    # TODO
-    pass
+    self.browser.get(self.URL)
 
   def search(self, phrase):
-    # TODO
-    pass
+    search_input = self.browser.find_element(*self.SEARCH_INPUT)
+    search_input.send_keys(phrase + Keys.RETURN)
