0932c6c324bbb2c0a0553b2bc0926941972014c9
AndyLPK247 <AndyLPK247@gmail.com>
2019-09-04 23:24:31 -0400

    handled race condition

--- tests/test_search.py
+++ tests/test_search.py
@@ -17,12 +17,13 @@ def test_basic_duckduckgo_search(browser):
   # When the user searches for "panda"
   search_page.search(PHRASE)
 
-  # Then the search result title contains "panda"
-  assert PHRASE in result_page.title()
-  
-  # And the search result query is "panda"
+  # Then the search result query is "panda"
   assert PHRASE == result_page.search_input_value()
   
   # And the search result links pertain to "panda"
   for title in result_page.result_link_titles():
     assert PHRASE.lower() in title.lower()
+
+  # And the search result title contains "panda"
+  # (Putting this assertion last guarantees that the page title will be ready)
+  assert PHRASE in result_page.title()
