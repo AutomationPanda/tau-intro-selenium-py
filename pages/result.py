c81740183478fd215b20fd56309b96ca3ff4d377
AndyLPK247 <AndyLPK247@gmail.com>
2019-09-04 20:06:43 -0400

    implemented result methods

--- pages/result.py
+++ pages/result.py
@@ -8,20 +8,27 @@ from selenium.webdriver.common.by import By
 
 class DuckDuckGoResultPage:
   
+  # Locators
+
   RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
   SEARCH_INPUT = (By.ID, 'search_form_input')
-  
+
+  # Initializer
+
   def __init__(self, browser):
     self.browser = browser
 
+  # Interaction Methods
+
   def result_link_titles(self):
-    # TODO
-    return []
+    links = self.browser.find_elements(*self.RESULT_LINKS)
+    titles = [link.text for link in links]
+    return titles
   
   def search_input_value(self):
-    # TODO
-    return ""
+    search_input = self.browser.find_element(*self.SEARCH_INPUT)
+    value = search_input.get_attribute('value')
+    return value
 
   def title(self):
-    # TODO
-    return ""
+    return self.browser.title
