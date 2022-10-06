1ba76ab3bac6174cfc544c3619e20f56893674a5
Carl Noval <cjrnoval22@gmail.com>
2022-10-07 02:29:30 +0900

    first test stub with gherkin test case

--- tests/test_fw.py
+++
@@ -1,9 +0,0 @@
-"""
-Run these tests after completing the setup steps to verify that the framework works.
-
-Result:
-- the command `pipenv run python -m pytest` executed without any issues
-"""
-
-def test_the_tests():
-  assert True
---
+++ tests/test_search.py
@@ -0,0 +1,22 @@
+"""
+These tests cover DuckDuckGo searches.
+"""
+
+def test_basic_duckduckgo_search():
+
+    # Given the DuckDuckGo home page is displayed
+    # TODO
+
+    # When the user searches for "panda"
+    # TODO
+
+    # Then the search result title contains "panda"
+    # TODO
+    
+    # And the search result query is "panda"
+    # TODO
+    
+    # And the search result links pertain to "panda"
+    # TODO
+
+    raise Exception("Incomplete Test")
