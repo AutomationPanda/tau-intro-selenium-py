de1c4fa123c27256c3a0123ebb5ce303fbf04749
alihaidar2950 <alihaidar2950@gmail.com>
2023-04-09 14:53:22 -0400

    Create result.py
    Create an object for our DuckDuckGo Result webpage

---
+++ pages/result.py
@@ -0,0 +1,22 @@
+"""
+This module contains DuckDuckGoResultPage,
+the page object for the DuckDuckGo search result page.
+"""
+
+
+class DuckDuckGoResultPage:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def result_link_titles(self):
+        # TODO
+        return []
+
+    def search_input_value(self):
+        # TODO
+        return ""
+
+    def title(self):
+        # TODO
+        return ""
