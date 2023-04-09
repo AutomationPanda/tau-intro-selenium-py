ac773500f72329952aaba118824bb63e219bc178
alihaidar2950 <alihaidar2950@gmail.com>
2023-04-09 14:52:39 -0400

    Create conftest.py
    A file where fixtures are defined to be used in our tests

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+"""
+This module contains shared fixtures.
+"""
+
+import pytest
+import selenium.webdriver
+
+
+@pytest.fixture
+def browser():
+
+  # Initialize the ChromeDriver instance
+  b = selenium.webdriver.Chrome()
+
+  # Make its calls wait up to 10 seconds for elements to appear
+  b.implicitly_wait(10)
+
+  # Return the WebDriver instance for the setup
+  yield b
+
+  # Quit the WebDriver instance for the cleanup
+  b.quit()
